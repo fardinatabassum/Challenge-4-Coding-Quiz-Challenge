d475961d39797ab474e3403d7e71846839c631da
fardinatabassum <fardinatabassum@gmail.com>
2023-01-19 05:40:42 -0500

    declred variables in js and made array

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,22 +8,55 @@ start_button.addEventListener("click", function() {
 
 }
 
+var quiz = document.getElementById(quiz)
+var questions = document.getElementById(question_text)
+var options = document.getElementById(options)
+var option1 = document.querySelector(".option-1")
+var option2 = document.querySelector(".option-2")
+var option3 = document.querySelector(".option-3")
+var option4 = document.querySelector(".option-4")
+var result = document.querySelector(".result")
 
-var questionsArr = [
+var quiz = [
     {
         question: "Commonly used data types DO Not Include:",
-        options: ["1.strings", "2.booleans", "3.alerts", "4.numbers"],
-        answer: "3.alert"
+        one: "strings",
+        two: "booleans",
+        three: "alerts",  
+        four: "numbers",
+        Answer: "3"
     },
     {
         question: "The condition in an if/else statement is enclosed with _______.",
-        options: ["1.quotes", "2.curly brackets", "3.parenthesis", "4.square brackets"],
-        answer: "2.curly brackets"
+        one: "quotes",
+        two: "curly brackets",
+        three: "parenthesis",  
+        four: "square brackers",
+        Answer: "2"
     },
+    
     {
         question: "Arrays in Javascript can be used to store _______.",
-        options: ["1.numbers and strings", "2.other arrays", "3.booleans", "4.all of the above"],
-        answer: "4.all of the above"
+        one: "numbers and strings",
+        two: "other arrays",
+        three: "booleans",  
+        four: "all if the above",
+        Answer: "4"
     },
-
+    {
+        question:"String values muust be enclosed within ______ when being assigned to variables.",
+        one: "commas",
+        two: "curly brackets",
+        three: "quotes",  
+        four: "parenthesis",
+        Answer: "3"
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        one: "JavaScript",
+        two: "terminal/bash",
+        three: "for loops",  
+        four: "console.log",
+        Answer: "4"
+    }
 ]
