fe3ba595f5a75e86612cf7abcdff93571be78afc
fardinatabassum <fardinatabassum@gmail.com>
2023-01-19 04:46:14 -0500

    added README, Basic HTML and started basic js and css

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+var start_button = document.querySelector(".start_button");
+var info_card = document.querySelector(".info_card");
+ 
+//start button clicked
+start_button.addEventListener("click", function() {
+    info_card
+})
+
+}
+
+
+var questionsArr = [
+    {
+        question: "Commonly used data types DO Not Include:",
+        options: ["1.strings", "2.booleans", "3.alerts", "4.numbers"],
+        answer: "3.alert"
+    },
+    {
+        question: "The condition in an if/else statement is enclosed with _______.",
+        options: ["1.quotes", "2.curly brackets", "3.parenthesis", "4.square brackets"],
+        answer: "2.curly brackets"
+    },
+    {
+        question: "Arrays in Javascript can be used to store _______.",
+        options: ["1.numbers and strings", "2.other arrays", "3.booleans", "4.all of the above"],
+        answer: "4.all of the above"
+    },
+
+]
